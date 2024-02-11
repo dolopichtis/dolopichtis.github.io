cc6a09d1adb430ca6c62032a69f95800422726bb
Dionisii <dolopichtis@gmail.com>
2024-02-11 16:53:32 +0200

    add banner

--- banner-test.js
+++ banner-test.js
@@ -1,19 +1,29 @@
+document.addEventListener('DOMContentLoaded', addBanner);
+
 function addBanner() {
-        let scarcityStudentsSource = 'https://enrollpro.io/scsorg/checkout/getStudentsNumber.php';
+    //settings:
+        let scarcityStudentsSource = 'https://enrollpro.io/scsorg/checkout/banner/getStudentsAjax.php';
+        
+    //add
         fetch(scarcityStudentsSource).then(studentsData => studentsData.text()).then(studentsNumber => makeBanner(studentsNumber));
 }
+
 function makeBanner(students) {
+        let cssSource = 'https://enrollpro.io/scsorg/checkout/banner/banner.css';
+    
         let body = document.body;
         let banner = document.createElement('div');
         banner.setAttribute('id', 'scarcity-banner');
         let text = document.createElement('h3');
         let message;
+        let link = document.createElement('a');
+        banner.appendChild(link);
+        link.setAttribute('href', 'https://southlandsonlineschools.com/schedule-a-call/');
+        link.innerHTML = 'Book a call';
         if (students > 0) {
-                message = `The number of available places to enroll is ${students} student${students > 1 ? 's' : ''}.`
-                let link = document.createElement('a');
+                message = `The number of available places to enroll is ${students} student${students > 1 ? 's' : ''}.`;
                 link.setAttribute('href', 'https://enrollpro.io/scsorg/checkout/');
                 link.innerHTML = 'Enroll now';
-                banner.appendChild(link);
         } else if (students === '0') {
                 message = 'Hi! We are full and no longer enrolling students at this time! Be sure to check back next month!';
         } else {
@@ -26,7 +36,16 @@ function makeBanner(students) {
         let cssLink = document.createElement('link');
         cssLink.rel = 'stylesheet';
         cssLink.type = 'text/css';
-        cssLink.href = 'https://enrollpro.io/scsorg/checkout/banner.css';
+        cssLink.href = cssSource;
         head.appendChild(cssLink);
+        
+        //remove button if checkout and students less than 1;
+        const isCheckout = document.location.pathname == '/scsorg/checkout/banner-test.php';
+        const button = document.querySelector('#submit-button');
+        const total = document.querySelector('.cc-cart__total');
+        if (isCheckout && students <= 0){
+            button.remove();
+            banner.setAttribute('class', 'silent');
+            total.remove();
+        }
 }
-document.addEventListener('DOMContentLoaded', addBanner);
